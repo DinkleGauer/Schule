412cadcc05362a5acd0b1d349a3821f3effd2b00
Alexey Badgauer <Alexey.Badgauer@HARTING.com>
2023-03-05 23:21:32 +0100

    GUI & Anfang

--- AngebotsVergleich/Form1.cs
+++ AngebotsVergleich/Form1.cs
@@ -1,7 +1,12 @@
+using System.Data.SqlClient;
+
+
 namespace AngebotsVergleich
 {
+
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
@@ -15,6 +20,17 @@ namespace AngebotsVergleich
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+
+        private void Erstellen_click(object sender, EventArgs e)
+        {
+            Angebot AngebotNEU = new Angebot();
+            AngebotNEU.Firma = textFirma.Text;
+            AngebotNEU.Bezugspreis = int.Parse(textBezugspreis.Text);
+            AngebotNEU.Lieferskonto = int.Parse(textLieferSkonto.Text);
+            AngebotNEU.Listeneinkaufspreis = int.Parse(textListenPreis.Text);
+            AngebotNEU.Lieferrabatt = int.Parse(textLieferRabatt.Text);
         }
     }
 }
