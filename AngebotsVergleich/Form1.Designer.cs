dc4d6fd367514d3f7532d3dbf3c47528dc7fe79e
104943 <104943@NBESPEL001259>
2023-03-06 12:47:33 +0100

    Etwas angefangen

--- AngebotsVergleich/Form1.Designer.cs
+++ AngebotsVergleich/Form1.Designer.cs
@@ -7,18 +7,20 @@
         public int Lieferrabatt { get; set; }
         public int Lieferskonto { get; set; }
         public int Bezugspreis { get; set; }
+        public int SonstRabatt { get; set; }
 
         public Angebot()
         {
 
         }
-        public Angebot(string Firma ,int Listeneinkaufspreis, int Lieferrabatt, int Lieferskonto, int Bezugspreis)
+        public Angebot(string Firma , int Listeneinkaufspreis, int Lieferrabatt, int Lieferskonto, int Bezugspreis, int SonstRabatt)
         {
             Firma = Firma;
             Listeneinkaufspreis = Listeneinkaufspreis;
             Lieferrabatt = Lieferrabatt;
             Lieferskonto = Lieferskonto;
             Bezugspreis = Bezugspreis;
+            SonstRabatt = SonstRabatt;
         }
     }
     partial class Form1
@@ -58,231 +60,175 @@
             this.textBezugspreis = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.AngebotErstellen = new System.Windows.Forms.Button();
-            this.textFirma = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textFirma2 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBezugspreis2 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textLieferRabatt2 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textLieferSkonto2 = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.textListenPreis2 = new System.Windows.Forms.TextBox();
-            this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textFirma = new System.Windows.Forms.TextBox();
+            this.textSonstRabatt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 113);
+            this.label1.Location = new System.Drawing.Point(33, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(133, 20);
+            this.label1.Size = new System.Drawing.Size(107, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Listeneinkaufspreis";
             // 
             // textListenPreis
             // 
-            this.textListenPreis.Location = new System.Drawing.Point(177, 110);
+            this.textListenPreis.Location = new System.Drawing.Point(155, 82);
+            this.textListenPreis.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textListenPreis.Name = "textListenPreis";
-            this.textListenPreis.Size = new System.Drawing.Size(125, 27);
+            this.textListenPreis.Size = new System.Drawing.Size(110, 23);
             this.textListenPreis.TabIndex = 2;
             // 
             // textLieferSkonto
             // 
-            this.textLieferSkonto.Location = new System.Drawing.Point(177, 192);
+            this.textLieferSkonto.Location = new System.Drawing.Point(155, 144);
+            this.textLieferSkonto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textLieferSkonto.Name = "textLieferSkonto";
-            this.textLieferSkonto.Size = new System.Drawing.Size(125, 27);
+            this.textLieferSkonto.Size = new System.Drawing.Size(110, 23);
             this.textLieferSkonto.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 192);
+            this.label2.Location = new System.Drawing.Point(33, 144);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.Size = new System.Drawing.Size(93, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "Lieferskonto (%)";
             // 
             // textLieferRabatt
             // 
-            this.textLieferRabatt.Location = new System.Drawing.Point(177, 151);
+            this.textLieferRabatt.Location = new System.Drawing.Point(155, 113);
+            this.textLieferRabatt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textLieferRabatt.Name = "textLieferRabatt";
-            this.textLieferRabatt.Size = new System.Drawing.Size(125, 27);
+            this.textLieferRabatt.Size = new System.Drawing.Size(110, 23);
             this.textLieferRabatt.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(38, 154);
+            this.label3.Location = new System.Drawing.Point(33, 116);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 20);
+            this.label3.Size = new System.Drawing.Size(88, 15);
             this.label3.TabIndex = 5;
             this.label3.Text = "Lieferrabatt (%)";
             // 
             // textBezugspreis
             // 
-            this.textBezugspreis.Location = new System.Drawing.Point(177, 233);
+            this.textBezugspreis.Location = new System.Drawing.Point(155, 175);
+            this.textBezugspreis.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBezugspreis.Name = "textBezugspreis";
-            this.textBezugspreis.Size = new System.Drawing.Size(125, 27);
+            this.textBezugspreis.Size = new System.Drawing.Size(110, 23);
             this.textBezugspreis.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(38, 233);
+            this.label4.Location = new System.Drawing.Point(33, 175);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.Size = new System.Drawing.Size(69, 15);
             this.label4.TabIndex = 7;
             this.label4.Text = "Bezugspreis";
             // 
             // AngebotErstellen
             // 
             this.AngebotErstellen.AutoSize = true;
-            this.AngebotErstellen.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.AngebotErstellen.Location = new System.Drawing.Point(254, 324);
+            this.AngebotErstellen.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AngebotErstellen.Location = new System.Drawing.Point(33, 273);
+            this.AngebotErstellen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.AngebotErstellen.Name = "AngebotErstellen";
-            this.AngebotErstellen.Size = new System.Drawing.Size(301, 75);
+            this.AngebotErstellen.Size = new System.Drawing.Size(232, 29);
             this.AngebotErstellen.TabIndex = 6;
-            this.AngebotErstellen.Text = "Vergleichen";
+            this.AngebotErstellen.Text = "Anlegen";
             this.AngebotErstellen.UseVisualStyleBackColor = true;
             this.AngebotErstellen.Click += new System.EventHandler(this.Erstellen_click);
-            // 
-            // textFirma
-            // 
-            this.textFirma.Location = new System.Drawing.Point(177, 69);
-            this.textFirma.Name = "textFirma";
-            this.textFirma.Size = new System.Drawing.Size(125, 27);
-            this.textFirma.TabIndex = 1;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(38, 69);
+            this.label6.Location = new System.Drawing.Point(33, 52);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 20);
+            this.label6.Size = new System.Drawing.Size(37, 15);
             this.label6.TabIndex = 12;
             this.label6.Text = "Firma";
-            // 
-            // textFirma2
-            // 
-            this.textFirma2.Location = new System.Drawing.Point(621, 69);
-            this.textFirma2.Name = "textFirma2";
-            this.textFirma2.Size = new System.Drawing.Size(125, 27);
-            this.textFirma2.TabIndex = 14;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(482, 69);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 20);
-            this.label5.TabIndex = 22;
-            this.label5.Text = "Firma";
-            // 
-            // textBezugspreis2
-            // 
-            this.textBezugspreis2.Location = new System.Drawing.Point(621, 233);
-            this.textBezugspreis2.Name = "textBezugspreis2";
-            this.textBezugspreis2.Size = new System.Drawing.Size(125, 27);
-            this.textBezugspreis2.TabIndex = 19;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(482, 233);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(88, 20);
-            this.label7.TabIndex = 21;
-            this.label7.Text = "Bezugspreis";
-            // 
-            // textLieferRabatt2
-            // 
-            this.textLieferRabatt2.Location = new System.Drawing.Point(621, 151);
-            this.textLieferRabatt2.Name = "textLieferRabatt2";
-            this.textLieferRabatt2.Size = new System.Drawing.Size(125, 27);
-            this.textLieferRabatt2.TabIndex = 16;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(482, 154);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(112, 20);
-            this.label8.TabIndex = 20;
-            this.label8.Text = "Lieferrabatt (%)";
-            // 
-            // textLieferSkonto2
-            // 
-            this.textLieferSkonto2.Location = new System.Drawing.Point(621, 192);
-            this.textLieferSkonto2.Name = "textLieferSkonto2";
-            this.textLieferSkonto2.Size = new System.Drawing.Size(125, 27);
-            this.textLieferSkonto2.TabIndex = 18;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(482, 192);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(116, 20);
-            this.label9.TabIndex = 17;
-            this.label9.Text = "Lieferskonto (%)";
-            // 
-            // textListenPreis2
-            // 
-            this.textListenPreis2.Location = new System.Drawing.Point(621, 110);
-            this.textListenPreis2.Name = "textListenPreis2";
-            this.textListenPreis2.Size = new System.Drawing.Size(125, 27);
-            this.textListenPreis2.TabIndex = 15;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(482, 113);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(133, 20);
-            this.label10.TabIndex = 13;
-            this.label10.Text = "Listeneinkaufspreis";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label11.Location = new System.Drawing.Point(93, 24);
+            this.label11.Location = new System.Drawing.Point(103, 14);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(121, 31);
+            this.label11.Size = new System.Drawing.Size(84, 25);
             this.label11.TabIndex = 23;
-            this.label11.Text = "Angebot 1";
+            this.label11.Text = "Angebot";
             // 
-            // label12
+            // dataGridView1
             // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label12.Location = new System.Drawing.Point(558, 24);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(121, 31);
-            this.label12.TabIndex = 24;
-            this.label12.Text = "Angebot 2";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(386, 52);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(270, 143);
+            this.dataGridView1.TabIndex = 24;
+            // 
+            // textFirma
+            // 
+            this.textFirma.Location = new System.Drawing.Point(155, 52);
+            this.textFirma.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textFirma.Name = "textFirma";
+            this.textFirma.Size = new System.Drawing.Size(110, 23);
+            this.textFirma.TabIndex = 1;
+            // 
+            // textSonstRabatt
+            // 
+            this.textSonstRabatt.Location = new System.Drawing.Point(155, 219);
+            this.textSonstRabatt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textSonstRabatt.Name = "textSonstRabatt";
+            this.textSonstRabatt.Size = new System.Drawing.Size(110, 23);
+            this.textSonstRabatt.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 15);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Sonst. Rabatt (%)";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(400, 273);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 29);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Vergleichen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Vergleichen_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.textFirma2);
+            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textSonstRabatt);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBezugspreis2);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.textLieferRabatt2);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.textLieferSkonto2);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.textListenPreis2);
-            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.textFirma);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.AngebotErstellen);
@@ -294,9 +240,11 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textListenPreis);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Angebots Vergleich";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -312,19 +260,12 @@
         private TextBox textBezugspreis;
         private Label label4;
         private Button AngebotErstellen;
-        private TextBox textFirma;
         private Label label6;
-        private TextBox textFirma2;
-        private Label label5;
-        private TextBox textBezugspreis2;
-        private Label label7;
-        private TextBox textLieferRabatt2;
-        private Label label8;
-        private TextBox textLieferSkonto2;
-        private Label label9;
-        private TextBox textListenPreis2;
-        private Label label10;
         private Label label11;
-        private Label label12;
+        private DataGridView dataGridView1;
+        private TextBox textFirma;
+        private TextBox textSonstRabatt;
+        private Label label5;
+        private Button button1;
     }
 }
--- AngebotsVergleich/Form1.cs
+++ AngebotsVergleich/Form1.cs
@@ -22,15 +22,67 @@ namespace AngebotsVergleich
 
         }
 
+        private void Felder_Leeren()
+        {
+            textBezugspreis.Clear();
+            textLieferRabatt.Clear();
+            textFirma.Clear();
+            textLieferSkonto.Clear();
+            textListenPreis.Clear();
+            textSonstRabatt.Clear();
+        }
 
+        
+
+        private string Inhalt_pruefen(string Inhalt, string Aufruf)
+        {
+            int Ergebnis;
+            bool success = int.TryParse(Inhalt, out Ergebnis);
+            if (success)
+            {
+                if (Ergebnis > 100 && (Aufruf == textSonstRabatt.Name || Aufruf == textLieferRabatt.Name || Aufruf == textLieferSkonto.Name))
+                {
+                    Felder_Leeren();
+                    System.Windows.Forms.MessageBox.Show("Ungueltige Eingabe oder mehr als 100%");
+                    return "0";
+                }
+                return Inhalt;
+            }
+            Felder_Leeren();
+            System.Windows.Forms.MessageBox.Show("Ungueltige Eingabe oder mehr als 100%");
+            return "0";
+        }
+
+        Angebot[] AngebotNEU = new Angebot[1];
         private void Erstellen_click(object sender, EventArgs e)
         {
-            Angebot AngebotNEU = new Angebot();
-            AngebotNEU.Firma = textFirma.Text;
-            AngebotNEU.Bezugspreis = int.Parse(textBezugspreis.Text);
-            AngebotNEU.Lieferskonto = int.Parse(textLieferSkonto.Text);
-            AngebotNEU.Listeneinkaufspreis = int.Parse(textListenPreis.Text);
-            AngebotNEU.Lieferrabatt = int.Parse(textLieferRabatt.Text);
+            // Daten bekommen
+            for (int i = AngebotNEU.Length-1; i < AngebotNEU.Length; i++)
+            {
+                AngebotNEU[i] = new Angebot
+                {
+                    Firma = textFirma.Text,
+                    Bezugspreis = int.Parse(textBezugspreis.Text),
+                    Lieferskonto = int.Parse(Inhalt_pruefen(textLieferSkonto.Text, textLieferSkonto.Name)),
+                    Listeneinkaufspreis = int.Parse(textListenPreis.Text),
+                    Lieferrabatt = int.Parse(Inhalt_pruefen(textLieferRabatt.Text, textLieferRabatt.Name)),
+                    SonstRabatt = int.Parse(Inhalt_pruefen(textSonstRabatt.Text, textSonstRabatt.Name))
+                };
+
+
+                // Füllen der Tabelle            
+                BindingSource binding = new BindingSource();
+                binding.DataSource = AngebotNEU;
+                dataGridView1.DataSource = binding;
+            }
+            System.Windows.Forms.MessageBox.Show("Angebot angelegt");
+            Felder_Leeren();
+            Array.Resize(ref AngebotNEU, AngebotNEU.Length +1);
+        }
+
+        private void Vergleichen_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
